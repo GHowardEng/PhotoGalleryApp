1c90fef5ff3e2cb213888cfc234fa855e60561fc
GHowardEng <60199078+GHowardEng@users.noreply.github.com>
2020-01-24 13:56:05 -0800

    Added scrolling functionality

--- app/src/main/java/com/example/photogalleryapp/MainActivity.java
+++ app/src/main/java/com/example/photogalleryapp/MainActivity.java
@@ -9,16 +9,24 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private int currentPhotoIndex = 0;
+    private ArrayList<String> photoGallery;
+    private String currentPhotoPath = null;
+    static Date minDate = new Date(Long.MIN_VALUE);
+    static Date maxDate = new Date(Long.MAX_VALUE);	// On startup, show all images
     String mCurrentPhotoPath;
 
     @Override
@@ -27,6 +35,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Render main activity
         setContentView(R.layout.activity_main);
+        Button btnLeft = (Button)findViewById(R.id.btnLeft);
+        Button btnRight = (Button)findViewById(R.id.btnRight);
+        btnLeft.setOnClickListener(this);
+        btnRight.setOnClickListener(this);
+
+        //Date minDate = new Date(Long.MIN_VALUE);
+        //Date maxDate = new Date(Long.MAX_VALUE);	// On startup, show all images
+        photoGallery = populateGallery(minDate, maxDate);	// Retrieve photos in date range
+        Log.d("onCreate, size", Integer.toString(photoGallery.size()));
+        if (photoGallery.size() > 0)
+            currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        displayPhoto(currentPhotoPath);
+    }
+    private View.OnClickListener filterListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            //Intent i = new Intent(MainActivity.this, SearchActivity.class);
+            //startActivityForResult(i, SEARCH_ACTIVITY_REQUEST_CODE);
+        }
+    };
+
+    private ArrayList<String> populateGallery(Date minDate, Date maxDate) {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath(), "/Android/data/com.example.photogalleryapp/files/Pictures");
+        photoGallery = new ArrayList<String>();
+        File[] fList = file.listFiles();
+        if (fList != null) {
+            for (File f : file.listFiles()) {
+                photoGallery.add(f.getPath());
+            }
+        }
+        return photoGallery;
+    }
+
+    private void displayPhoto(String path) {
+        ImageView iv = (ImageView) findViewById(R.id.ivGallery);
+        iv.setImageBitmap(BitmapFactory.decodeFile(path));
+    }
+
+    public void onClick( View v) {
+        switch (v.getId()) {
+            case R.id.btnLeft:
+                --currentPhotoIndex;
+                break;
+            case R.id.btnRight:
+                ++currentPhotoIndex;
+                break;
+            default:
+                break;
+        }
+        if (currentPhotoIndex < 0)
+            currentPhotoIndex = 0;
+        if (currentPhotoIndex >= photoGallery.size())
+            currentPhotoIndex = photoGallery.size() - 1;
+        if(photoGallery.size() > 0) {
+            currentPhotoPath = photoGallery.get(currentPhotoIndex);
+            Log.d("phpotoleft, size", Integer.toString(photoGallery.size()));
+            Log.d("photoleft, index", Integer.toString(currentPhotoIndex));
+            displayPhoto(currentPhotoPath);
+        }
     }
 
     public void takePicture(View v) {
@@ -48,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    public File createImageFile() throws IOException {
+  /*  public File createImageFile() throws IOException {
         // Generate empty JPEG file with timestamp
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -57,16 +124,32 @@ public class MainActivity extends AppCompatActivity {
         File image = File.createTempFile(imageFileName, ".jpg",storageDir);
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
-    }
+    }*/
 
     // This method is for event handling - specifically the event of returning from the camera activity
     // at this point the picture should be displayed in the image view
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Add new picture to gallery and set date range (may need to be changed for filtering)
+            populateGallery(minDate, maxDate);
+            // Set photo index to display newest image
+            currentPhotoIndex = photoGallery.size() - 1;
+            // Get path of newest image
+            currentPhotoPath = photoGallery.get(currentPhotoIndex);
+            // Set image view to display newest image
             ImageView mImageView = findViewById(R.id.ivGallery);
-            mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+            mImageView.setImageBitmap(BitmapFactory.decodeFile(currentPhotoPath));
         }
     }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", dir );
+        currentPhotoPath = image.getAbsolutePath();
+        Log.d("createImageFile", currentPhotoPath);
+        return image;
+    }
 }
-// test comment for test commit
